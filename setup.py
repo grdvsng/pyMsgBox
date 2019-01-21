c0999f810172dbf31eb41f1cb487eb4c389ed6a8
Sergei Trishkin <43265300+grdvsng@users.noreply.github.com>
2019-01-21 12:51:20 +0300

    setup.py
    Setup tools.

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'Module create VBS MsgBox and return vbValue.',
+    'author': 'Sergey Trishkin',
+    'url': 'https://github.com/grdvsng/pyMsgBox',
+    'download_url': 'https://drive.google.com/drive/folders/1mr5o5t4KpzyjqhxwxPhttP7x9NagP316?usp=sharing',
+    'author_email': 'grdvsng@gmail.com',
+    'version': '1.0',
+    'install_requires': ['nose'],
+    'packages': ['pyMsgBox'],
+    'scripts': ['bin\\'],
+    'name': 'pyvoc',
+    }
+
+setup(**config)
